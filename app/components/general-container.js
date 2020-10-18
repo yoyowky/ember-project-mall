2e68f7175b0261bcb8aafc937932d477ac490ba5
Kaiyu Wang <cathywky123@gmail.com>
2020-10-17 21:15:46 -0400

    form input & @tracked class.object

--- app/components/general-container.js
+++ app/components/general-container.js
@@ -3,4 +3,10 @@ import { inject as servise } from '@ember/service';
 
 export default class GeneralContainerComponent extends Component {
     @servise shoppingCart;
+
+    get itemCount(){
+        return this.shoppingCart.itemList.reduce((total, item) => {
+            return total += item.count;
+        }, 0);
+    }
 }
--- app/controllers/cart.js
+++ app/controllers/cart.js
@@ -1,13 +1,14 @@
 import Controller from '@ember/controller';
 import { inject as servise } from '@ember/service';
+import { action } from '@ember/object';
 
 export default class CartController extends Controller {
     @servise shoppingCart;
     // @servise('shopping-cart') cart; // cart is a customise name 
 
     get subtotal(){
-        return this.model.reduce((acc, item) => {
-            return acc + item.price;
+        return this.shoppingCart.itemList.reduce((acc, item) => {
+            return acc + item.price * item.count;
             }, 0);
     }
     get tax(){
@@ -15,5 +16,15 @@ export default class CartController extends Controller {
     }
     get total(){
         return this.subtotal + this.tax;
+    }
+
+    @action
+    updateItemCount(item, event){   // item是传进来的参数， event是action
+        const count = event.target.value; //input框里的数字
+        if(count>=0){
+            item.count = count;
+        } else {
+            item.count = 0;
+        } 
     }
 }
